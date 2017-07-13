744d286ff71d4f8137ede7e534a87b724a3643fd
Scott Flischel <scott.flischel@gmail.com>
2017-07-13 16:45:51 -0400

    Removed tab and tabSet components and added doctype to about.component.html

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,8 +1,10 @@
 import angular from 'angular';
-    import ListTablesModule from './listTables/listTables.module';    import ListsModule from './lists/lists.module';    import ListsFormModule from './listsForm/listsForm.module';    import NewListFormModule from './newListForm/newListForm.module';    import PageHeaderModule from './pageHeader/pageHeader.module';    import TabModule from './tab/tab.module';    import TabSetModule from './tabSet/tabSet.module';    import AboutModule from './about/about.module';
+    import AboutModule from './about/about.module';    import ListTablesModule from './listTables/listTables.module';    import ListsModule from './lists/lists.module';    import ListsFormModule from './listsForm/listsForm.module';    import NewListFormModule from './newListForm/newListForm.module';    import PageHeaderModule from './pageHeader/pageHeader.module';
 
 const ComponentsModule = angular.module('app.components',[
-       ListTablesModule.name,      ListsModule.name,      ListsFormModule.name,      NewListFormModule.name,      PageHeaderModule.name,      TabModule.name,      TabSetModule.name,      AboutModule.name 
+       AboutModule.name,      ListTablesModule.name,      ListsModule.name,      ListsFormModule.name,      NewListFormModule.name,      PageHeaderModule.name 
 ]);
 
 export default ComponentsModule;
+
+  
--- client/app/components/tab/tab.module.js
+++
@@ -1,7 +0,0 @@
-import angular from 'angular';
-import tabComponent from './tab.component';
-import tabSet from '../tabSet/tabSet.module';
-
-const tabModule = angular.module('tab', ['tabSet'])
-  .component('tab', tabComponent);
-export default tabModule;
